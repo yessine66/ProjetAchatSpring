45a012f8fd6ae74a2677f5885d77efa72726c902
Your Name <mohamedaziz.bennasr@esprit.tn>
2022-11-18 09:22:32 +0100

    update nexus

--- src/main/java/tn/esprit/rh/achat/services/FournisseurServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/FournisseurServiceImpl.java
@@ -74,8 +74,8 @@ public class FournisseurServiceImpl implements IFournisseurService {
 
 	@Override
 	public void assignSecteurActiviteToFournisseur(Long idSecteurActivite, Long idFournisseur) {
-		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
-		SecteurActivite secteurActivite = secteurActiviteRepository.findById(idSecteurActivite).orElse(null);
+		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElseThrow(NullPointerException::new);
+		SecteurActivite secteurActivite = secteurActiviteRepository.findById(idSecteurActivite).orElseThrow(NullPointerException::new);
         fournisseur.getSecteurActivites().add(secteurActivite);
         fournisseurRepository.save(fournisseur);
 		
