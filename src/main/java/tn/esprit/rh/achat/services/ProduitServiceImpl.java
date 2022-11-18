05435f937ea45e4c273e1bfe055efdd24c59038a
Your Name <mohamedaziz.bennasr@esprit.tn>
2022-11-18 09:53:55 +0100

    update vs

--- src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
@@ -60,7 +60,7 @@ public class ProduitServiceImpl implements IProduitService {
 	@Override
 	public void assignProduitToStock(Long idProduit, Long idStock) {
 		Produit produit = produitRepository.findById(idProduit).orElseThrow(NullPointerException::new);
-		Stock stock = stockRepository.findById(idStock).orElse(null);
+		Stock stock = stockRepository.findById(idStock).orElseThrow(NullPointerException::new);
 		produit.setStock(stock);
 		produitRepository.save(produit);
 
